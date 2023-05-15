1d0c94205681cfad02ad295048a3097bbc392e99
Francisco Arce <darce@teledolar.com>
2023-05-15 17:14:08 -0600

    fix(Client): Aislamiento de recursos Auth. FirstPage

--- TN.Client.Components.Authentication/Pages/FirstPage.razor.cs
+++ TN.Client.Components.Authentication/Pages/FirstPage.razor.cs
@@ -6,11 +6,43 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TN.Client.Services.Shared.Interfaces;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TN.Client.Components.Authentication.Pages
 {
     public class FirstPageBase : ComponentBase
     {
         [Inject] protected ILocalizerService LocalizerService { get; set; }
+        [Inject] protected IApplicationInformation ApplicationInformationService { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
+
+        public bool showTopButton { get; set; }
+        public string applicationName { get; set; } = "";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            applicationName = ApplicationInformationService.GetApplicationName();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await Task.Delay(1000).ContinueWith((_) => showTopButton = true);
+                StateHasChanged();
+            }
+        }
+
+        protected async Task NavigationToLogin()
+        {
+            NavigationManager.NavigateTo("/login", true);
+        }
+
+        protected async Task ChangeTextToEnglish()
+        {
+            LocalizerService.SetSpecificCulture("en");
+
+        }
     }
 }
--- TN.Client.Services.Shared/Implementations/Shared/LocalizerService.cs
+++ TN.Client.Services.Shared/Implementations/Shared/LocalizerService.cs
@@ -25,20 +25,19 @@ namespace TN.Client.Services.Shared.Implementations.Shared
 
         public LocalizerService(IOptions<ApplicationServiceOptions> options)
         {
-            var culture = $"{options?.Value?.Language}-{options?.Value?.Culture}"; 
+            var culture = string.IsNullOrEmpty(options?.Value?.Culture) ? options?.Value?.Language : $"{options?.Value?.Language}-{options?.Value?.Culture}"; 
             SetSpecificCulture(culture);
         }
         
         public string GetResourceValue(string key,[CallerFilePathAttribute]string @namespace = null)
         {
             var method = new StackTrace().GetFrame(1).GetMethod();
-            string className = method.DeclaringType.Name; 
-            string namespaceName = method.DeclaringType.Namespace;
+            //string className = method.DeclaringType.Name; 
+            //string namespaceName = method.DeclaringType.Namespace;
+            //string namespaceNameBase = method.DeclaringType.BaseType.ToString();
 
             var assembly = Assembly.GetCallingAssembly();
-            ////TODO: 
-            var baseName = "TN.Client.Components.Authentication.Pages.FirstPageBase";
-            //var baseName = $"{namespaceName}.{className}
+            var baseName = method.DeclaringType.BaseType.ToString();
 
             _resourceManager = new ResourceManager(baseName, assembly);
             return _resourceManager.GetString(key, _culture) ?? "Resource Not Found!";
