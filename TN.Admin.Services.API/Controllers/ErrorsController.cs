ce586c0eb3d89d5c06acf745551b94e40b0e972f
Daimler <dvalverde@teledolar.net>
2023-05-18 05:40:38 -0600

    fix(Admin): :bug: Ambiguo metodo HTTP Error Swagger
    Se comenta metodo ErrorsController.Error ya que genera un error a la hora de generar la documentacion de Swagger, esto por ambiguedad de metodo de Error ya existente.

--- TN.Admin.Services.API/Controllers/ErrorsController.cs
+++ TN.Admin.Services.API/Controllers/ErrorsController.cs
@@ -5,11 +5,11 @@ namespace TN.Admin.Services.API.Controllers
 {
 	public class ErrorsController : ControllerBase
 	{
-		[Route("/error")]
-		public IActionResult Error()
-		{
-			Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-			return Problem(detail: exception?.Message, instance: HttpContext?.Request?.Path);
-		}
+		// [Route("/error")]
+		// public IActionResult Error()
+		// {
+		// 	Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+		// 	return Problem(detail: exception?.Message, instance: HttpContext?.Request?.Path);
+		// }
 	}
 }
